ae7fcddeab4e4a87c8a3a9fac5996a57e3aa5147
EdwinXMorocho <125777395+EdwinXMorocho@users.noreply.github.com>
2025-06-30 21:51:16 -0500

    Update securityConfig

--- src/main/java/com/trabajo/posappseguras/config/SecurityConfig.java
+++ src/main/java/com/trabajo/posappseguras/config/SecurityConfig.java
@@ -1,4 +1,33 @@
 package com.trabajo.posappseguras.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .headers(headers -> headers
+                        .contentTypeOptions(Customizer.withDefaults()) // Agrega la cabecera X-Content-Type-Options: nosniff
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::deny) // Agrega la cabecera X-Frame-Options: DENY
+                        .httpStrictTransportSecurity(hstsConfig ->
+                                hstsConfig.maxAgeInSeconds(31536000)
+                        )
+                )
+                .authorizeHttpRequests(authz -> authz // Configuración de autorización de peticiones HTTP
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
 }
